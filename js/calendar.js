ca56cad791a6012313b02c5f3c3db220a0f7e6de
nsohail <nsohail92@gmail.com>
2015-01-30 15:11:04 -0500

    test

--- js/calendar.js
+++ js/calendar.js
@@ -30,7 +30,7 @@ function handleAuthResult(authResult) {
 
 function handleAuthClick(event) {
   // Step 3: get authorization to use private data
-  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
   return false;
 }
 
