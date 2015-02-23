5c516fbc2b2ca9bde4671b8440311dd0dea53886
nsohail <nsohail92@gmail.com>
2015-02-23 12:16:30 -0500

    Added current location for weather

--- js/main.js
+++ js/main.js
@@ -1,75 +1,114 @@
 $(document).ready(function () {
 
+	getLocation();
+	formatTime();
+	formatDate();
+});
 
+var currentLocation;
+
+//time area
+
+function formatTime(){
+	var date = new Date();
+	var hours = date.getHours();
+	//console.log("this is the hour time" + hours);
+	var minutes = date.getMinutes();
+	//console.log("this is the minutes time" + minutes);
+
+	var ampm = hours >= 12 ? 'pm' : 'am';
+	minutes = minutes < 10 ? '0'+minutes : minutes;
+	hours = hours % 12;
+	hours = hours ? hours : 12; //explain this
+	var displayTime = hours + ':' + minutes + ' ' + ampm;
+	//console.log(displayTime);
+	$('.row1').find($('.current-time')).text(displayTime);
+
+	window.setTimeout (function(){
+		formatTime();
+	}, 10*1000);
+}
+
+
+
+//date area
+
+function formatDate(){
 	var currentDate = new Date();
 
-	function formatTime(){
-		var date = new Date();
-		var hours = date.getHours();
-		//console.log("this is the hour time" + hours);
-		var minutes = date.getMinutes();
-		//console.log("this is the minutes time" + minutes);
+	var date = new Date();
+	var weekday = new Array();
+	weekday[0]=  "Sunday";
+	weekday[1] = "Monday";
+	weekday[2] = "Tuesday";
+	weekday[3] = "Wednesday";
+	weekday[4] = "Thursday";
+	weekday[5] = "Friday";
+	weekday[6] = "Saturday";
 
-		var ampm = hours >= 12 ? 'pm' : 'am';
-		minutes = minutes < 10 ? '0'+minutes : minutes;
-		hours = hours % 12;
-		hours = hours ? hours : 12; //explain this
-		var displayTime = hours + ':' + minutes + ' ' + ampm;
-		//console.log(displayTime);
-		$('.row1').find($('.current-time')).text(displayTime);
+	var month = new Array();
+	month[0] = "Jan";
+	month[1] = "Feb";
+	month[2] = "March";
+	month[3] = "April";
+	month[4] = "May";
+	month[5] = "June";
+	month[6] = "July";
+	month[7] = "Aug";
+	month[8] = "Sept";
+	month[9] = "Oct";
+	month[10] = "Nov";
+	month[11] = "Dec";
 
-		window.setTimeout (function(){
-			formatTime();
-		}, 10*1000);
+	var displayDay = weekday[date.getDay()];
+	var displayMonth = month[date.getMonth()];
+	var displayDate = currentDate.getDate();
+
+	var displayWholeDate = (displayDay + ', ' + displayMonth + ' ' + displayDate);
+	$('.row1').find($('.current-day')).text(displayWholeDate);
+
+	window.setTimeout(function(){
+		formatDate();
+	}, 1*60*1000);
+}
+
+
+
+
+//weather area
+
+function getLocation(){
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(gotLocation,initialize);
+	}
+	else{
+		alert("Device does not support geolocation");
+		initialize();
+	}
+}
+
+//get the location coords
+function gotLocation(pos){
+	var crd = pos.coords;
+	currentLocation = loadWeather(crd.latitude+','+crd.longitude);
+	initialize();
+}
+
+function initialize(){
+	if(!currentLocation){
+		loadWeather("Washington, DC");
+		console.log("Loaded DC weather");
 	}
 
-	formatTime();
-
-	function formatDate(){
-		var date = new Date();
-		var weekday = new Array();
-		weekday[0]=  "Sunday";
-		weekday[1] = "Monday";
-		weekday[2] = "Tuesday";
-		weekday[3] = "Wednesday";
-		weekday[4] = "Thursday";
-		weekday[5] = "Friday";
-		weekday[6] = "Saturday";
-
-		var month = new Array();
-		month[0] = "Jan";
-		month[1] = "Feb";
-		month[2] = "March";
-		month[3] = "April";
-		month[4] = "May";
-		month[5] = "June";
-		month[6] = "July";
-		month[7] = "Aug";
-		month[8] = "Sept";
-		month[9] = "Oct";
-		month[10] = "Nov";
-		month[11] = "Dec";
-
-		var displayDay = weekday[date.getDay()];
-		var displayMonth = month[date.getMonth()];
-		var displayDate = currentDate.getDate();
-
-		var displayWholeDate = (displayDay + ', ' + displayMonth + ' ' + displayDate);
-		$('.row1').find($('.current-day')).text(displayWholeDate);
-
-		window.setTimeout(function(){
-			formatDate();
-		}, 10*1000);
+	else{
+		loadWeather(currentLocation);
+		console.log("Loaded current weather");
 	}
+}
 
-	formatDate();
-
-
-
-
-
+function loadWeather(location){
 	$.simpleWeather({
-		location: 'Washington, DC',
+		location: location,
 		woeid: '',
 		unit: 'f',
 		success: function(weather) {
@@ -81,6 +120,8 @@ $(document).ready(function () {
 		  $(".current-weather").html('<p>'+error+'</p>');
 		}
 	});
+}
 	
-});
+	
+
 	
